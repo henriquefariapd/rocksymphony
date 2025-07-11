b5c6330f13327c3e39514f7616b0a4824716b8a6
TrackThTrail <45440895+TrackThTrail@users.noreply.github.com>
2025-07-11 02:08:31 -0300

    acerta dist

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -78,8 +78,10 @@ app.add_middleware(
         "http://localhost:5173", 
         "http://localhost:3000",
         "http://127.0.0.1:5173",
-        "http://127.0.0.1:3000"
-    ],  # URLs do frontend
+        "http://127.0.0.1:3000",
+        "https://rocksymphony-3f7b8e8b3afd.herokuapp.com",
+        "http://rocksymphony-3f7b8e8b3afd.herokuapp.com"
+    ],  # URLs do frontend (local e produção)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
